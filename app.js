74281c3b5e47d66a6eb21023dc50a9790fac2fb7
EzequielJavaS <68125532+EzequielJavaS@users.noreply.github.com>
2021-03-16 20:07:00 +0100

    50 Menú con Inquirer

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
 require('colors');
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 console.clear();
 
 const main = async() => {
     let opt = '';
     do {
-        opt = await mostrarMenu(); // Await= Espérate a que se ejecute esto.
+        opt = await inquirerMenu() ; // Await= Espérate a que se ejecute esto.
         console.log({ opt });
-        ( opt !== '0')&& await pausa(); //Hace que pida un ENTER para continuar.
+
+        await pausa();
     } while( opt !=='0');
 }
 
