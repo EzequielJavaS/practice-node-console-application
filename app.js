e97533e1c590b2d5dfff083470b58005beedcf0e
EzequielJavaS <68125532+EzequielJavaS@users.noreply.github.com>
2021-03-22 12:17:31 +0100

    51 Logica manejo de tareas hecho

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
 require('colors');
 
 console.clear();
@@ -7,7 +8,7 @@ const main = async() => {
     let opt = '';
     do {
         opt = await inquirerMenu() ; // Await= Espérate a que se ejecute esto.
-        console.log({ opt });
+        console.log( {opt} );
 
         await pausa();
     } while( opt !=='0');
---
+++ models/tareas.js
@@ -0,0 +1,14 @@
+
+/**
+ *  _listado:
+ *      {'uuid-123456-125554-2: {id:12, desc:loQuSea, completadoEn: 94457}}
+ *      {'uuid-123456-125554-2: {id:13, desc:loQuSea, completadoEn: 94457}}
+ */
+class Tareas {
+    _listado = {};
+
+    constructor(){
+        this._listado = {}
+    }
+}
+module.exports = Tareas;
